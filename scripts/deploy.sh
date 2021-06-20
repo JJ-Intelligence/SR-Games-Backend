be5a7e71cfa7f98b6e2b6011a5b01d289405b682
Josh Hook <joshhook@live.co.uk>
2021-06-20 20:55:56 +0100

    Updated deploy version to v2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 export PROJECT_ID=jjgames
-export TAG=v1
+export TAG=v2
 docker build -f Dockerfile -t gcr.io/$PROJECT_ID/sr-games-backend:$TAG .
 gcloud services enable containerregistry.googleapis.com
 gcloud auth configure-docker
