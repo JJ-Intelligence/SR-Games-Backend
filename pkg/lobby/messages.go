61952164e810c11d4e2baddd9aea5d90b632b927
Josh Hook <joshhook@live.co.uk>
2021-08-22 23:14:09 +0100

    Moved lobby code into separate file and created Lobby request worker

---
+++ pkg/lobby/messages.go
@@ -0,0 +1,31 @@
+package lobby
+
+// Lobby Player management
+type LobbyJoinRequest struct {
+	PlayerID string `json:"playerID"`
+	LobbyID  string `json:"lobbyID"`
+}
+
+type PlayerJoinedEvent struct{}
+
+type LobbyLeaveRequest struct{}
+
+type PlayerLeftEvent struct{}
+
+type LobbyPlayerListBroadcast struct {
+	PlayerIDs []string `json:"playerIDs"`
+}
+
+// Starting a Game
+type LobbyStartGameRequest struct {
+	Game string `json:"game"`
+}
+
+type LobbyStartGameStatusResponse struct {
+	Status bool   `json:"status"`
+	Reason string `json:"reason"`
+}
+
+type LobbyStartGameBroadcast struct {
+	Game string `json:"game"`
+}
